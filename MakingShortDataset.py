9d479f8b068209a22fbb1e2161e35349512f942f
Mohameddnabil <Mohamed927Nabil@gmail.com>
2022-09-08 15:48:16 +0200

    removed image

--- MakingShortDataset.py
+++
@@ -1,7 +0,0 @@
-
-
-from moviepy.video.io.ffmpeg_tools import ffmpeg_extract_subclip
-from moviepy.video.io.VideoFileClip import VideoFileClip
-with VideoFileClip(r"D:\Dataset\Double\Videos\Game1.MP4") as video:
-    new = video.subclip(0,15)
-    new.write_videofile(f"D:\Dataset\Double\Videos\Game1small.mp4", audio_codec='aac')
